6c171e4bdcb57b5cfb6d5e940d0f7455a122acbf
James Jeffries <jamesjeffries1@gmail.com>
2011-12-16 19:44:12 +0000

    Added formal argument parser

--- strfmt.py
+++ strfmt.py
@@ -5,6 +5,23 @@ def runFormat(fmt, args):
 		print fmt % arg
 
 if __name__ == "__main__":
+	import argparse
+
+	parser = argparse.ArgumentParser(description="A command line string formatter")
+	parser.add_argument('-r', 
+	                    action="store_true", 
+			    default=False,
+			    help="run in range mode")
+	parser.add_argument('-f',
+	                    action="store"
+			    help="string format")
+	parser.add_argument('-a',
+	                    action="store"
+			    help="arguments for string format")
+	parser.add_argument('-v', '--version', action='version')
+
+	args = parser.parse_args(sys.argv)
+
 	if len(sys.argv) < 3:
 		print "Usage: strfmt <format> <args>"
 		sys.exit(1)
