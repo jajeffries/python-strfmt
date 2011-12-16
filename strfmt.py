796ce5861da71458814b67aee39a51508117471d
James Jeffries <jamesjeffries1@gmail.com>
2011-12-16 20:22:44 +0000

    Added range lookup

--- strfmt.py
+++ strfmt.py
@@ -1,8 +1,25 @@
 import sys
 
 def runFormat(fmt, args, rangeMode=False):
-	for arg in args:
-		print fmt % arg
+	if rangeMode:
+		rangeFormat(fmt, args)
+	else:
+		normalFormat(fmt, args)
+
+def normalFormat(fmt, args):
+	print fmt % args
+
+def rangeFormat(fmt, args):
+	if '-' in args:
+		#Assuming comma seperated list
+		splitArgs = args.split('-')
+		if len(splitArgs) != 2:
+			print "Format arguments are in an invalid format for range use"
+		for x in xrange(int(splitArgs[0]), int(splitArgs[1])+1):
+			print fmt % x
+	else:
+		print "Format arguments are in an invalid format for range use"
+
 
 if __name__ == "__main__":
 	import argparse
